170e7eb0aceb2e8f214fdd62736a9671c90dc554
yosuke.yuasa <yosuke.yuasa@rakuten.com>
2018-06-01 00:54:46 +0000

    add column

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -48,8 +48,15 @@ class TasksController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
-    {
+    {   
+        $this->validate($request, [
+            'status' => 'required',   
+            'content' => 'required',
+        ]);
+        
+        
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -96,7 +103,13 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [
+            'status' => 'required',   
+            'content' => 'required',
+        ]);
+        
         $task = Task::find($id);
+        $task->status = $request->status; 
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,7 +5,13 @@
 <!-- Write content for each page here -->
 <h1>タスク新規作成ページ</h1>
 
+ 
+
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
+
 
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
